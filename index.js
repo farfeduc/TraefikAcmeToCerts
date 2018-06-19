da89bf52999e6b044bbd4016faf64d4fe760aff6
Adrien Girardeau <adrigir25@gmail.com>
2018-06-19 15:20:22 +0200

    Imp: support new version of acme.json in newer versions of Traefik.

--- index.js
+++ index.js
@@ -2,22 +2,38 @@ var fs = require("fs");
 var path = require("path");
 var acme = require(__dirname + "/acme/acme.json");
 
-if (typeof(acme) !== 'undefined' && (!acme.hasOwnProperty("DomainsCertificate") ||
-    !acme.DomainsCertificate.hasOwnProperty("Certs"))) {
+function removeWildcard(domain){
+  if(domain.indexOf("*") > -1) {
+    return domain.replace('*', 'wildcard');
+  }
+  else {
+    return domain
+  }
+}
+
+function writeCertsFiles(domain, cert, key){
+  var certDump = new Buffer(cert, 'base64');
+  var keyDump = new Buffer(key, 'base64');
+
+  fs.writeFileSync(path.join(__dirname, "certs/" + domain + ".crt"), certDump);
+  fs.writeFileSync(path.join(__dirname, "certs/" + domain + ".key"), keyDump);
+}
+
+if (typeof(acme) === 'undefined' || (!acme.hasOwnProperty("Certificates") && !acme.hasOwnProperty("DomainsCertificate")) ) {
     console.log("Nothing to do");
 }
 else {
-  if(typeof(acme) !== 'undefined'){
+  if(typeof(acme) !== 'undefined' && acme.hasOwnProperty("DomainsCertificate")){
     for (var cert of acme.DomainsCertificate.Certs) {
-      let domain = cert.Certificate.Domain;
-
-      var certDump = new Buffer(cert.Certificate.Certificate, 'base64');
-      var keyDump = new Buffer(cert.Certificate.PrivateKey, 'base64');
-
-      fs.writeFileSync(path.join(__dirname, "certs/" + domain + ".crt"), certDump);
-      fs.writeFileSync(path.join(__dirname, "certs/" + domain + ".key"), keyDump);
+      let domain = removeWildcard(cert.Certificate.Domain);
+      writeCertsFiles(domain, cert.Certificate.Certificate, cert.Certificate.PrivateKey);
     }
-
-    console.log("Successfully write all your acme certs & keys");
   }
+  if (typeof(acme) !== 'undefined' && acme.hasOwnProperty("Certificates")){
+    for (var cert of acme.Certificates) {
+      let domain = removeWildcard(cert.Domain.Main);
+      writeCertsFiles(domain, cert.Certificate, cert.Key);
+    }
+  }
+  console.log("Successfully write all your acme certs & keys");
 }
